9c715d6e9662ea8652f6d202467a09593c72081c
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-11-17 14:57:13 +0100

    finished frontend

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ $(".navbar-toggler-icon").click(function(){
     $(".nav-menu").toggleClass("hidden");
 });    
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({items:1,autoplay:true,loop:true});
-  });
+    $("#index-s1-owl").owlCarousel({items:1,autoplay:true,loop:true});
+    $("#booklet-owl, #poster-owl").owlCarousel({items:2,autoplay:true,loop:true});
+    $("#graphics-owl, #video-owl").owlCarousel({items:3,autoplay:true,loop:true});
+});
